7af7d664f65fa437c8b3cdd57a71d944ba11198b
banbanzhong-art <banbanzhong-art@users.noreply.github.com>
2026-07-22 18:49:13 +0200

    Publish hero circle removal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hainan-2026-v10";
+const CACHE_NAME = "hainan-2026-v11";
 const CORE_ASSETS = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
