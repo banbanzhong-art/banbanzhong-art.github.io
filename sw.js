80ada440d9c40e2e0bb964cb66c0ffa9e0320e28
banbanzhong-art <banbanzhong@gmail.com>
2026-07-25 21:54:46 +0200

    Simplify mobile hero labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hainan-2026-v18";
+const CACHE_NAME = "hainan-2026-v19";
 const CORE_ASSETS = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
