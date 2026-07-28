b2154da6f152843b920b21049a21c0098a7b4f4a
banbanzhong-art <banbanzhong@gmail.com>
2026-07-28 15:48:46 +0200

    Keep selected calendar day below sticky header

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hainan-2026-v23";
+const CACHE_NAME = "hainan-2026-v24";
 const CORE_ASSETS = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
